cc6deba3b723fdacfc197d1cbc20d5f048bd84aa
Jericho <jerichobantiquete@gmail.com>
2022-07-31 23:40:14 +0800

    Adding Next.js SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,16 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Header } from '../components/Header';
 import { Hero } from '../components/Hero';
+import { sanityClient, urlFor } from '../sanity';
+import { Post } from '../typings';
 
-const Home: NextPage = () => {
+interface IHomeProps {
+  posts: Post[];
+}
+
+const Home: NextPage<IHomeProps> = ({ posts }) => {
+  console.log(posts);
+
   return (
     <div>
       <Head>
@@ -18,3 +26,25 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const query = `*[ _type == "post"]{
+  _id,
+  title,
+  author -> {
+    name,
+    image
+  },
+  description,
+  mainImage,
+  slug
+}`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
